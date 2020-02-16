99c166cd1b8cdafa1e528732d201b324b00d1ad4
Shashikant Chaudhary <mailshashi122@gmail.com>
2020-02-16 17:37:08 +0530

    pypi changes

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='arbdlib',
-    version='1.0',
+    version='1.01',
     packages=['Arbd'],
-    url='https://github.com/hatchnhack/arbd1-lib',
+    url='https://github.com/hatchnhack/arbd-lib',
     license='GPL-3.0',
     author='Shashikant Chaudhary',
     author_email='contact@hatchnhack.com',
